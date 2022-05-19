5f6fbf4bedf4b86635361e7bf6a51b0685e08b38
Ümit Aslan <prototprotot2@gmail.com>
2022-05-19 16:46:24 +0300

    Trap codes are generated

--- Assets/Scripts/TrapControl.cs
+++ Assets/Scripts/TrapControl.cs
@@ -6,19 +6,28 @@ public class TrapControl : MonoBehaviour
 {
     public GameObject parent;
     StackManager stackManager;
-     void Start() {
+    Mover mover;
+    void Start() {
         stackManager = FindObjectOfType<StackManager>();
+        mover = FindObjectOfType<Mover>();
     }
    private void OnCollisionEnter(Collision other) {
        if (other.gameObject.CompareTag("trap"))
        {
           Debug.Log("Trap");
+          mover.animator.SetBool("Impact", true);
+          mover.animator.SetBool("Idling", false);
+          mover.animator.SetBool("Run", false);
+          mover.transform.position = Vector3.MoveTowards(transform.position,Vector3.Lerp(Vector3.back, transform.position, 0.5f), mover.speed * Time.deltaTime);
           StartCoroutine(IClearItems());
           
        } 
    }
    private void OnCollisionExit(Collision other) {
        stackManager.prevObject=GameObject.Find("Stack1").transform;
+        mover.animator.SetBool("Impact", false);
+        mover.animator.SetBool("Idling", true);
+        mover.animator.SetBool("Run", false);
    }
    private IEnumerator IClearItems()
      {
