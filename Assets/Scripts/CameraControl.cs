72f0440f1f788c81afd2ddee127179fa096e14fd
Ümit Aslan <prototprotot2@gmail.com>
2022-05-28 21:23:10 +0300

    Comment lines added

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -9,7 +9,7 @@ public class CameraControl : MonoBehaviour
     public float lerpValue;
     void LateUpdate()
     {
-        Vector3 desPos = target.position + offset;
-        transform.position = Vector3.Lerp(transform.position, desPos, lerpValue);
+        Vector3 desPos = target.position + offset;//Hedefin pozisyonu + offset'i al.
+        transform.position = Vector3.Lerp(transform.position, desPos, lerpValue);//Hedefin pozisyonu ile transform'in pozisyonunu lerp'le.
     }
 }
