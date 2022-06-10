af5ab87d1a1287f51be2008a3039a04726401285
Ümit Aslan <79724156+umit-aslan@users.noreply.github.com>
2022-06-10 09:12:25 +0300

    Comment lines added

--- Assets/Scripts/Game_Manager.cs
+++ Assets/Scripts/Game_Manager.cs
@@ -17,34 +17,32 @@ public class Game_Manager : MonoBehaviour
    private void Start()
    {
        Time.timeScale = 0;
-       audioSource = GetComponent<AudioSource>();
-       collectPoint = FindObjectOfType<CollectPoint>();
+       audioSource = GetComponent<AudioSource>();//AudioSource'ı alır
+       collectPoint = FindObjectOfType<CollectPoint>();//CollectPoint scriptini alır
    }
    public void GameStart()
    {
         Time.timeScale = 1;
-        startPanel.SetActive(false);
+        startPanel.SetActive(false);//StartPanel'ı kapat
    }
    public void GameRestart()
    {
-        SceneManager.LoadScene(0);  
+        SceneManager.LoadScene(0);//Scene'i yeniden yükler
    }
 
    public void GameWin()
    {
         GameWinPanelScoreText.GetComponent<Text>().text = "Score: " + collectPoint.counter.ToString();
-        GameWinPanel.SetActive(true);
+        GameWinPanel.SetActive(true);//GameWinPanel'ı aktif hale getirir
         Time.timeScale = 0;
-        audioSource.PlayOneShot(audioClips[0]);
+        audioSource.PlayOneShot(audioClips[0]);//GameWin sesini çalar
    }
 
    public void GameOver()
    {
-        GameOverPanelScoreText.GetComponent<Text>().text = "Score: " + collectPoint.counter.ToString();
-        GameOverPanel.SetActive(true);
+        GameOverPanelScoreText.GetComponent<Text>().text = "Score: " + collectPoint.counter.ToString();//GameOverPanelScoreText'e skoru yazdırır
+        GameOverPanel.SetActive(true);//GameOverPanel'ı aktif hale getirir
         Time.timeScale = 0;
-        audioSource.PlayOneShot(audioClips[1]);
+        audioSource.PlayOneShot(audioClips[1]);//GameOver sesini çalar
    }
-
-
 }
